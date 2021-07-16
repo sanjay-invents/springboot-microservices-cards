dc535d8e22bd5f633fcbc6602a864c854cadaf1a
sanjay-invents <sanjay.invents@gmail.com>
2021-07-16 09:12:08 +0545

    Course completed

--- src/main/java/com/shoryukane/cards/controller/CardsController.java
+++ src/main/java/com/shoryukane/cards/controller/CardsController.java
@@ -8,16 +8,17 @@ import com.shoryukane.cards.model.Cards;
 import com.shoryukane.cards.model.Customer;
 import com.shoryukane.cards.model.Properties;
 import com.shoryukane.cards.repository.CardsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class CardsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
 
     @Autowired
     private CardsRepository cardsRepository;
@@ -26,7 +27,8 @@ public class CardsController {
     private CardsServiceConfig cardsServiceConfig;
 
     @PostMapping("/myCards")
-    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+    public List<Cards> getCardDetails(@RequestHeader("shoryukane-correlation-id") String correlationId, @RequestBody Customer customer) {
+        logger.info("Inside my cards method.");
         return cardsRepository.findByCustomerId(customer.getCustomerId());
     }
 
